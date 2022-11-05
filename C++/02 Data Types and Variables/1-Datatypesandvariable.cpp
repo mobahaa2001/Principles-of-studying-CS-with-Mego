ab21c3c054bf04d602dc68c355ff3c65f4cb0a0f
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-11-05 05:52:31 +0200

    Add files via upload

---
+++ C++/02 Data Types and Variables/1-Datatypesandvariable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Data Types
+    // 1- Integer is used with Number
+    int age = 21;
+    // 2- Double(or Float) is used with Number
+    double myWeight = 66.7;
+    // 3- Character is used with letter
+    char myLetter = 'M';
+    // 4- String/ Sequence of Characters are used with Word
+    string myName = "Mego";
+    // 5- Boolean is used with True or False
+    bool status = true;
+    /////////////////////////////////////
+    cout << age;
+}
