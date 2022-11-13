cca90de903c81e1de1c5eef0c7c8bbe116c53f1c
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-11-13 16:05:26 +0200

    Add files via upload

---
+++ C++/02 Data Types and Variables/HomeWork/Easy Challenges/1-Homework_Answer.cpp
@@ -0,0 +1,44 @@
+/*
+Homework 1: Math operations
+Write a program that reads 2 numbers and print their + - * / as following
+
+Input
+12 3
+
+Output
+12 + 3 = 15
+12 - 3 = 9
+12 / 3 = 4
+12 * 3 = 36
+
+Do good testing for your code
+E.g. consider zero as first or 2nd number
+E.g. consider negative values
+E.g. even and odd values
+E.g. try the MAX of int: 2147483647
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cin >> a >> b;
+    // 12 3
+    cout << a << " + " << b << " = " << a + b << "\n"; // 15
+    cout << a << " - " << b << " = " << a - b << "\n"; // 9
+    cout << a << " / " << b << " = " << a / b << "\n"; // 5
+    cout << a << " * " << b << " = " << a * b << "\n"; // 36
+
+    return 0;
+}
+/*
+Try inputs
+
+12 3
+15 0
+0 15
+2147483647 2147483647
+*/
