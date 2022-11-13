5c6eaaa6fa3dd7c3bd71bebb3e974e3c53252afc
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-11-14 00:52:44 +0200

    Add files via upload

---
+++ C++/HackerRank/2.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=a+b+c;
+    cout<<d;
+
+    return 0;
+}
