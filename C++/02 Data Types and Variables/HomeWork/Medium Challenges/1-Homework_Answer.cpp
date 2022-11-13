9fa73368659b1e9fac0fb1740990e5997c731735
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-11-14 00:05:51 +0200

    Add files via upload

---
+++ C++/02 Data Types and Variables/HomeWork/Medium Challenges/1-Homework_Answer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, num3;
+
+	num1 = 0, num2 = 1, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+	num1 = num2, num2 = num3, num3 = num1 + num2, cout <<num3<<"\n";
+
+	return 0;
+}
