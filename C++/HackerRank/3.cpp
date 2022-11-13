a0f9178eb706ec3359981ad87b1605c97a3a1586
Mego <70282836+mobahaa2001@users.noreply.github.com>
2022-11-14 00:55:03 +0200

    Add files via upload

---
+++ C++/HackerRank/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int x; long y;
+    char ch;
+    float a; double b;
+    scanf("%d %ld %c %f %lf",&x ,&y ,&ch ,&a ,&b);
+    printf("%d\n%ld\n%c\n%f\n%lf",x ,y ,ch ,a ,b);
+    return 0;
+}
